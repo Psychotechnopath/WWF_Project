f08c14a0f8007801dc8e976e9880e78d2d1cb029
RodgervanderHeijden <rodgervdheijden@hotmail.com>
2020-04-04 18:01:58 +0200

    Added (partially copied) method to compare number of nearest neighbours parameter, based on Cohen's kappa. Results as of now are shit due to sample size, will hopefully become useful with bigger sample.

--- ADASYN.py
+++ ADASYN.py
@@ -20,6 +20,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score
 from imblearn.metrics import geometric_mean_score
 import xgboost as xgb
+import winsound
 
 # Import data
 with open("./partial_pickle_25.pkl", "rb") as f:
@@ -42,7 +43,7 @@ print(y_test.value_counts())
 # The meat of the file: naive train-test-split + xgboost fitting
 # Input are the training data, either in original shape or resampled
 # The testing data are identical for every pipeline
-def split_and_xgb(x_resampled, y_resampled):
+def train_xgb(x_resampled, y_resampled):
     xgb_model = xgb.XGBClassifier(objective='reg:squarederror', random_state=47)
     xgb_model.fit(x_resampled, y_resampled)
     #xgb_model.save_model('trained_model')
@@ -57,16 +58,16 @@ def split_and_xgb(x_resampled, y_resampled):
 
 
 # # First pipeline: just all data unedited
-print("No resampling")
-split_and_xgb(x_train, y_train)
-
-# Second pipeline: only ADASYN oversampling to minority == 10% of data
-print("ADASYN 0.1 oversampling")
-over_2 = ADASYN(sampling_strategy=0.1)
-steps_2 = [ ('o', over_2)]
-pipeline_2 = Pipeline(steps_2)
-x_r_2, y_r_2 = pipeline_2.fit_resample(x_train, y_train)
-split_and_xgb(x_r_2, y_r_2)
+# print("No resampling")
+# train_xgb(x_train, y_train)
+#
+# # Second pipeline: only ADASYN oversampling to minority == 10% of data
+# print("ADASYN 0.1 oversampling")
+# over_2 = ADASYN(sampling_strategy=0.1)
+# steps_2 = [ ('o', over_2)]
+# pipeline_2 = Pipeline(steps_2)
+# x_r_2, y_r_2 = pipeline_2.fit_resample(x_train, y_train)
+# train_xgb(x_r_2, y_r_2)
 #
 # # Third pipeline: only SMOTE oversampling to minority == 10% of data
 # print("SMOTE 0.1 oversampling")
@@ -74,7 +75,7 @@ split_and_xgb(x_r_2, y_r_2)
 # steps_3 = [ ('o', over_3)]
 # pipeline_3 = Pipeline(steps_3)
 # x_r_3, y_r_3 = pipeline_3.fit_resample(x_train, y_train)
-# split_and_xgb(x_r_3, y_r_3)
+# train_xgb(x_r_3, y_r_3)
 #
 # # Third pipeline: only SMOTE oversampling to minority == 10% of data
 # print("SMOTE 0.3 oversampling")
@@ -82,7 +83,7 @@ split_and_xgb(x_r_2, y_r_2)
 # steps_3 = [ ('o', over_3)]
 # pipeline_3 = Pipeline(steps_3)
 # x_r_3, y_r_3 = pipeline_3.fit_resample(x_train, y_train)
-# split_and_xgb(x_r_3, y_r_3)
+# train_xgb(x_r_3, y_r_3)
 #
 # # Third pipeline: only SMOTE oversampling to minority == 10% of data
 # print("SMOTE 0.5 oversampling")
@@ -90,7 +91,7 @@ split_and_xgb(x_r_2, y_r_2)
 # steps_3 = [ ('o', over_3)]
 # pipeline_3 = Pipeline(steps_3)
 # x_r_3, y_r_3 = pipeline_3.fit_resample(x_train, y_train)
-# split_and_xgb(x_r_3, y_r_3)
+# train_xgb(x_r_3, y_r_3)
 #
 # # Fourth pipeline: only randomly undersampling to minority == 10% of data
 # print("RUS 0.1")
@@ -98,7 +99,7 @@ split_and_xgb(x_r_2, y_r_2)
 # steps_4 = [ ('u', under_4)]
 # pipeline_4 = Pipeline(steps_4)
 # x_r_4, y_r_4 = pipeline_4.fit_resample(x_train, y_train)
-# split_and_xgb(x_r_4, y_r_4)
+# train_xgb(x_r_4, y_r_4)
 #
 # # Fifth pipeline: both ADASYN oversampling and random undersampling
 # # such that min class == 10% of data
@@ -108,7 +109,7 @@ split_and_xgb(x_r_2, y_r_2)
 # steps_5 = [ ('o', over_5), ('u', under_5)]
 # pipeline_5 = Pipeline(steps_5)
 # x_r_5, y_r_5 = pipeline_5.fit_resample(x_train, y_train)
-# split_and_xgb(x_r_5, y_r_5)
+# train_xgb(x_r_5, y_r_5)
 #
 # # Sixth pipeline: both SMOTE oversampling and random undersampling
 # # such that min class == 10% of data
@@ -118,14 +119,83 @@ split_and_xgb(x_r_2, y_r_2)
 # steps_6 = [ ('o', over_6), ('u', under_6)]
 # pipeline_6 = Pipeline(steps_6)
 # x_r_6, y_r_6 = pipeline_6.fit_resample(x_train, y_train)
-# split_and_xgb(x_r_6, y_r_6)
+# train_xgb(x_r_6, y_r_6)
+#
+# for j in [5, 10]: #nearest neighbors
+#     for i in [0.5, 0.6]:
+#         print("ADASYN sampling ratio {}".format(i))
+#         print("ADASYN knn {}".format(j))
+#         adasyn = ADASYN(sampling_strategy=i, n_neighbors=j)
+#         steps = [('u', adasyn)]
+#         pipeline = Pipeline(steps)
+#         x_r, y_r = pipeline.fit_resample(x_train, y_train)
+#         train_xgb(x_r, y_r)
 
-for j in [3, 5, 10]: #nearest neighbors
-    for i in [0.4, 0.5, 0.6]:
-        print("ADASYN sampling ratio {}".format(i))
-        print("ADASYN knn {}".format(j))
-        adasyn = ADASYN(sampling_strategy=i, n_neighbors=j)
-        steps = [('u', adasyn)]
-        pipeline = Pipeline(steps)
-        x_r, y_r = pipeline.fit_resample(x_train, y_train)
-        split_and_xgb(x_r, y_r)
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import model_selection as ms
+from sklearn import datasets, metrics, tree
+
+from imblearn import over_sampling as os
+from imblearn import pipeline as pl
+
+RANDOM_STATE = 42
+
+scorer = metrics.make_scorer(metrics.cohen_kappa_score)
+
+adasyn = os.ADASYN(random_state=RANDOM_STATE)
+xgb = xgb.XGBClassifier(random_state=RANDOM_STATE)
+pipeline = pl.make_pipeline(adasyn, xgb)
+winsound.Beep(500, 100)
+
+#         print("ADASYN knn {}".format(j))
+#         adasyn = ADASYN(sampling_strategy=i, n_neighbors=j)
+#         steps = [('u', adasyn)]
+#         pipeline = Pipeline(steps)
+#         x_r, y_r = pipeline.fit_resample(x_train, y_train)
+#
+
+
+
+param_range = range(1, 12)
+train_scores, test_scores = ms.validation_curve(
+    pipeline, X, y, param_name="adasyn__n_neighbors", param_range=param_range,
+    cv=3, scoring=scorer, n_jobs=1)
+train_scores_mean = np.mean(train_scores, axis=1)
+train_scores_std = np.std(train_scores, axis=1)
+test_scores_mean = np.mean(test_scores, axis=1)
+test_scores_std = np.std(test_scores, axis=1)
+
+winsound.Beep(500, 300)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+plt.plot(param_range, test_scores_mean, label='ADASYN')
+ax.fill_between(param_range, test_scores_mean + test_scores_std,
+                test_scores_mean - test_scores_std, alpha=0.2)
+idx_max = np.argmax(test_scores_mean)
+plt.scatter(param_range[idx_max], test_scores_mean[idx_max],
+            label=r'Cohen Kappa: ${:.2f}\pm{:.2f}$'.format(
+                test_scores_mean[idx_max], test_scores_std[idx_max]))
+
+plt.title("Validation Curve with ADASYN-XGB")
+plt.xlabel("n_neighbors")
+plt.ylabel("Cohen's kappa")
+
+# make nice plotting
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.get_xaxis().tick_bottom()
+ax.get_yaxis().tick_left()
+ax.spines['left'].set_position(('outward', 10))
+ax.spines['bottom'].set_position(('outward', 10))
+plt.xlim([1, 10])
+plt.ylim([0.4, 0.8])
+
+plt.legend(loc="best")
+plt.show()
+winsound.Beep(500, 3000)
