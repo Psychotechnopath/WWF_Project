84ab780a50b5be742e85f4238fe001d854df44b0
RodgervanderHeijden <rodgervdheijden@hotmail.com>
2020-04-15 10:27:58 +0200

    update adasyn plus (smote)tomek.py toegevoegd

--- ADASYN.py
+++ ADASYN.py
@@ -22,12 +22,43 @@ from imblearn.metrics import geometric_mean_score
 import xgboost as xgb
 import winsound
 
+winsound.Beep(500, 1000)
+
 # Import data
-with open("./partial_pickle_25.pkl", "rb") as f:
-    df = pd.DataFrame(pickle.load(f))
+with open("D:/Downloads/DEiA/TilePickle_25.pkl", "rb") as f:
+    tile = pickle.load(f)
+
+    # Flatten matrix
+    reshaped_tile = tile.reshape(tile.shape[0], (tile.shape[1] * tile.shape[2]))
+    tile = 0
+
+    # Transform to dataframe
+    df = pd.DataFrame(reshaped_tile.T, columns=['AggIndex1', 'AggIndex2', 'AggIndex3',
+                                                'AggIndex4', 'AggIndex5', 'EdgeDensity1',
+                                                'EdgeDensity2', 'EdgeDensity3', 'EdgeDensity4',
+                                                'PatchDensity1', 'PatchDensity2', 'PatchDensity3',
+                                                'PatchDensity4', 'LandcoverPercentage1', 'LandcoverPercentage2',
+                                                'LandcoverPercentage3', 'LandcoverPercentage4', 'ShannonDiversity',
+                                                'current_deforestationDistance', 'current_degradationDistance',
+                                                'future_deforestation', 'RawSarVisionClasses', 'SarvisionBasemap',
+                                                'scaledPopDensity', 'scaledASTER', 'RoadsDistance',
+                                                'UrbanicityDistance',
+                                                'WaterwaysDistance', 'CoastlineDistance', 'MillDistance',
+                                                'PalmOilConcession',
+                                                'gradientASTER', 'LogRoadDistance', 'Vegetype', 'CurrentMonth',
+                                                'y_center',
+                                                'x_center', 'time', 'size'])
+    reshaped_tile = 0
     # Just a small check
     print(df.shape)
     print(df['future_deforestation'].value_counts())
+
+#
+# with open("./partial_pickle_25.pkl", "rb") as f:
+#     df = pd.DataFrame(pickle.load(f))
+#     # Just a small check
+#     print(df.shape)
+#     print(df['future_deforestation'].value_counts())
 
 # Memory issues are basically non-existent, so df can keep existing :)
 y = df['future_deforestation']
@@ -120,82 +151,84 @@ def train_xgb(x_resampled, y_resampled):
 # pipeline_6 = Pipeline(steps_6)
 # x_r_6, y_r_6 = pipeline_6.fit_resample(x_train, y_train)
 # train_xgb(x_r_6, y_r_6)
+
+for j in [10]: #nearest neighbors
+    for i in [0.5]:
+        print("ADASYN sampling ratio {}".format(i))
+        print("ADASYN knn {}".format(j))
+        adasyn = ADASYN(sampling_strategy=i, n_neighbors=j)
+        steps = [('u', adasyn)]
+        pipeline = Pipeline(steps)
+        x_r, y_r = pipeline.fit_resample(x_train, y_train)
+        train_xgb(x_r, y_r)
+
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from sklearn import model_selection as ms
+# from sklearn import datasets, metrics, tree
 #
-# for j in [5, 10]: #nearest neighbors
-#     for i in [0.5, 0.6]:
-#         print("ADASYN sampling ratio {}".format(i))
+# from imblearn import over_sampling as os
+# from imblearn import pipeline as pl
+#
+# RANDOM_STATE = 42
+#
+# scorer = metrics.make_scorer(metrics.cohen_kappa_score)
+#
+# adasyn = os.ADASYN(random_state=RANDOM_STATE)
+# xgb = xgb.XGBClassifier(random_state=RANDOM_STATE)
+# pipeline = pl.make_pipeline(adasyn, xgb)
+# winsound.Beep(500, 100)
+
 #         print("ADASYN knn {}".format(j))
 #         adasyn = ADASYN(sampling_strategy=i, n_neighbors=j)
 #         steps = [('u', adasyn)]
 #         pipeline = Pipeline(steps)
 #         x_r, y_r = pipeline.fit_resample(x_train, y_train)
-#         train_xgb(x_r, y_r)
-
-
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import model_selection as ms
-from sklearn import datasets, metrics, tree
-
-from imblearn import over_sampling as os
-from imblearn import pipeline as pl
-
-RANDOM_STATE = 42
-
-scorer = metrics.make_scorer(metrics.cohen_kappa_score)
-
-adasyn = os.ADASYN(random_state=RANDOM_STATE)
-xgb = xgb.XGBClassifier(random_state=RANDOM_STATE)
-pipeline = pl.make_pipeline(adasyn, xgb)
-winsound.Beep(500, 100)
-
-#         print("ADASYN knn {}".format(j))
-#         adasyn = ADASYN(sampling_strategy=i, n_neighbors=j)
-#         steps = [('u', adasyn)]
-#         pipeline = Pipeline(steps)
-#         x_r, y_r = pipeline.fit_resample(x_train, y_train)
 #
 
 
-
-param_range = range(1, 12)
-train_scores, test_scores = ms.validation_curve(
-    pipeline, X, y, param_name="adasyn__n_neighbors", param_range=param_range,
-    cv=3, scoring=scorer, n_jobs=1)
-train_scores_mean = np.mean(train_scores, axis=1)
-train_scores_std = np.std(train_scores, axis=1)
-test_scores_mean = np.mean(test_scores, axis=1)
-test_scores_std = np.std(test_scores, axis=1)
-
-winsound.Beep(500, 300)
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-
-plt.plot(param_range, test_scores_mean, label='ADASYN')
-ax.fill_between(param_range, test_scores_mean + test_scores_std,
-                test_scores_mean - test_scores_std, alpha=0.2)
-idx_max = np.argmax(test_scores_mean)
-plt.scatter(param_range[idx_max], test_scores_mean[idx_max],
-            label=r'Cohen Kappa: ${:.2f}\pm{:.2f}$'.format(
-                test_scores_mean[idx_max], test_scores_std[idx_max]))
-
-plt.title("Validation Curve with ADASYN-XGB")
-plt.xlabel("n_neighbors")
-plt.ylabel("Cohen's kappa")
-
-# make nice plotting
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.get_xaxis().tick_bottom()
-ax.get_yaxis().tick_left()
-ax.spines['left'].set_position(('outward', 10))
-ax.spines['bottom'].set_position(('outward', 10))
-plt.xlim([1, 10])
-plt.ylim([0.4, 0.8])
-
-plt.legend(loc="best")
-plt.show()
-winsound.Beep(500, 3000)
+# param_range = range(1, 12)
+# train_scores, test_scores = ms.validation_curve(
+#     pipeline, X, y, param_name="adasyn__n_neighbors", param_range=param_range,
+#     cv=3, scoring=scorer, n_jobs=1)
+#
+#
+#
+# train_scores_mean = np.mean(train_scores, axis=1)
+# train_scores_std = np.std(train_scores, axis=1)
+# test_scores_mean = np.mean(test_scores, axis=1)
+# test_scores_std = np.std(test_scores, axis=1)
+#
+# winsound.Beep(500, 300)
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1)
+#
+# plt.plot(param_range, test_scores_mean, label='ADASYN')
+# ax.fill_between(param_range, test_scores_mean + test_scores_std,
+#                 test_scores_mean - test_scores_std, alpha=0.2)
+# idx_max = np.argmax(test_scores_mean)
+# plt.scatter(param_range[idx_max], test_scores_mean[idx_max],
+#             label=r'Cohen Kappa: ${:.2f}\pm{:.2f}$'.format(
+#                 test_scores_mean[idx_max], test_scores_std[idx_max]))
+#
+# plt.title("Validation Curve with ADASYN-XGB")
+# plt.xlabel("n_neighbors")
+# plt.ylabel("Cohen's kappa")
+#
+# # make nice plotting
+# ax.spines['top'].set_visible(False)
+# ax.spines['right'].set_visible(False)
+# ax.get_xaxis().tick_bottom()
+# ax.get_yaxis().tick_left()
+# ax.spines['left'].set_position(('outward', 10))
+# ax.spines['bottom'].set_position(('outward', 10))
+# plt.xlim([1, 10])
+# plt.ylim([0.4, 0.8])
+#
+# plt.legend(loc="best")
+# plt.show()
+# winsound.Beep(500, 3000)
