9674afe5669df9796bd7cfe2c1c74c47845b20f1
Tim J <timjongenelen@hotmail.com>
2020-04-16 10:03:34 +0200

    Bootstrap framework aangemaakt (for loop, de twee parameters erboven, en import resample) + spaties weggehaald in column names

--- RAND_UNDERSAMP.py
+++ RAND_UNDERSAMP.py
@@ -9,7 +9,7 @@ from xgboost import XGBClassifier
 path = d2g.set_path_base('Tim') + 'TilePickle_25.pkl'
 df = d2g.to_dataframe(path)
 
-if False:  # Rodger z'n df downsize
+if False:  # Rodger z'n df downsize (heb ik nodig om deze code op mijn pc te runnen - TJ)
     # Select all 1000 cases uit 27174 where y is positive
     sample_pos = df[df['future_deforestation'] == 1].sample(n = 10, random_state=47, replace=False)
     # And for the 1000:1 ratio, select 1000x as many cases where y is negative
@@ -22,7 +22,7 @@ X = df.copy(deep=True)
 X.drop(columns=['future_deforestation'], inplace=True)
 y = df['future_deforestation']
 
-over = imb.over_sampling.RandomOverSampler(random_state=42)
+over = imb.over_sampling.RandomOverSampler(sampling_strategy=0.10, random_state=42)
 under = imb.under_sampling.RandomUnderSampler(sampling_strategy=0.33)
 steps = [('o', over), ('u', under)]
 pipeline = imb.pipeline.Pipeline(steps)
