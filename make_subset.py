9130715eafa6bd424c5bedc32b1ded3ab0777b4b
RodgervanderHeijden <rodgervdheijden@hotmail.com>
2020-05-11 16:44:07 +0200

    Merge branch 'master' of C:\Users\Rodger\Documents\Data Science\DS&E\Git\WWF_Project with conflicts.

--- make_subset.py
+++ make_subset.py
@@ -2,11 +2,13 @@ from deia2_general import set_path_base, to_dataframe
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-path = set_path_base("Yme")
-df = to_dataframe(f"{path}/TilePickle_25.pkl")
+path = set_path_base("Rodger")
+#df = to_dataframe(f"{path}/TilePickle_25.pkl")
 
+
+path = set_path_base("Rodger")
 # Deze aanpassen naar welke files je op je laptoppie heb staan
-files_on_my_laptop = [8, 9, 15, 16, 17, 24, 25, 26]
+files_on_my_laptop = list(range(0, 31))
 paths = [f'{path}TilePickle_{file}.pkl' for file in files_on_my_laptop]
 
 #this for loop constructs the actual subset (as a dataframe)
@@ -14,17 +16,33 @@ subset_x = pd.DataFrame()
 subset_y = pd.DataFrame()
 
 for index, pkl in enumerate(paths):
+    print(index, pkl)
     #For every pickle in the path list make a Dataframe
     df = to_dataframe(pkl)
-    df.dropna(inplace=True)
-    y = df['future_deforestation']
-    df.drop(columns=['future_deforestation'], inplace=True)
-    X = df
-    # Abuse train_test_split somewhat to generate stratified subsamples of data
-    x_outsample, x_insample, y_outsample, y_insample = train_test_split(X, y, test_size=0.0003, stratify=y, random_state=47)
-    # Appending sub-samples to a complete dataframe which is a subset over all pickles.
-    subset_x = subset_x.append(x_insample)
-    subset_y = subset_y.append(y_insample.to_frame())
+    print(len(df))
+    if len(df.dropna()) > 5000:
+        print("niet lege df")
+        df.dropna(inplace=True)
+        print(len(df))
+        y = df['future_deforestation']
+        # This is our X
+        # To not brick my laptop, I'm not gonna save it twice
+        df.drop(columns=['future_deforestation'], inplace=True)
+        # X = df
+        print(len(y), len(df))
+        # Abuse train_test_split somewhat to generate stratified subsamples of data
+        x_outsample, x_insample, y_outsample, y_insample = train_test_split(df, y, train_size=0.0003, test_size=0.0003,
+                                                                            stratify=y, random_state=47)
+        df = 0
+        y = 0
+        # Appending sub-samples to a complete dataframe which is a subset over all pickles.
+        subset_x = subset_x.append(x_insample)
+        subset_y = subset_y.append(y_insample.to_frame())
+    else:
+        print("lege of bijna lege df")
+        df.dropna(inplace=True)
+        print(len(df))
+
 
 #Reset indices so we have an increasing index again. We don't want the old indices so we drop them
 subset_x.reset_index(inplace=True, drop=True)
