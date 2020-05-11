b4d02d67c730e65c648ee43ad881edc545900049
Tim J <timjongenelen@hotmail.com>
2020-05-11 21:10:36 +0200

    Random undersampling definitief versie 2.0 -Nieuwe d2g methodes erin verwerkt -Mooie selectie gemaakt voor under, over, of allebei-samplen

--- RAND_UNDERSAMP.py
+++ RAND_UNDERSAMP.py
@@ -1,41 +1,40 @@
-import pandas as pd
 import deia2_general as d2g
 import pickle
 import imblearn as imb
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
-from xgboost import XGBClassifier
 
-path = d2g.set_path_base('Tim') + 'TilePickle_25.pkl'
-df = d2g.to_dataframe(path)
-
-if False:  # Rodger z'n df downsize (voor de zwakkere pc's onder ons)
-    # Select all 1000 cases uit 27174 where y is positive
-    sample_pos = df[df['future_deforestation'] == 1].sample(n = 10, random_state=47, replace=False)
-    # And for the 1000:1 ratio, select 1000x as many cases where y is negative
-    sample_neg = df[df['future_deforestation'] == 0].sample(n = 10000, random_state=47, replace=False)
-    # Add these two parts together in a new df
-    df = pd.concat([sample_pos, sample_neg])
-
-
+path = d2g.set_path_base('Tim') + 'subset_x.pkl'
+with open(path, "rb") as f:
+    df = pickle.load(f)
+path = d2g.set_path_base('Tim') + 'subset_y.pkl'
+with open(path, "rb") as f:
+    y = pickle.load(f)
 X = df.copy(deep=True)
-X.drop(columns=['future_deforestation'], inplace=True)
-y = df['future_deforestation']
+y = y['future_deforestation']
 
-over = imb.over_sampling.RandomOverSampler(sampling_strategy=0.10, random_state=42)
-under = imb.under_sampling.RandomUnderSampler(sampling_strategy=0.33)
-steps = [('o', over), ('u', under)]
-pipeline = imb.pipeline.Pipeline(steps)
-X, y = pipeline.fit_resample(X, y)
-x_train, x_test, y_train, y_test = train_test_split (X, y, test_size=0.25, random_state=47)
 
-xgb_model = XGBClassifier(objective='reg:squarederror', random_state=47)
-xgb_model.fit(x_train, y_train)
-y_pred = xgb_model.predict(x_test)
-print(y_pred.max())
+sampler_choice = 2  # 1 = Under, 2 = Over, 3 = Both
 
-score = accuracy_score(y_test, y_pred, normalize=True)
-conf_matrix = confusion_matrix(y_test, y_pred)
+if sampler_choice == 1:
+    under = imb.under_sampling.RandomUnderSampler(sampling_strategy=0.04166666666)  # 4% minority after resampling
+    steps = [('u', under)]
+    pipeline = imb.pipeline.Pipeline(steps)
+    X, y = pipeline.fit_resample(X, y)
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    d2g.xg_boost(x_train, y_train, x_test, y_test, 'Random Undersampling')
+elif sampler_choice == 2:
+    over = imb.over_sampling.RandomOverSampler(sampling_strategy=0.04166666666)  # 4% minority after resampling
+    steps = [('o', over)]
+    pipeline = imb.pipeline.Pipeline(steps)
+    X, y = pipeline.fit_resample(X, y)
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    d2g.xg_boost(x_train, y_train, x_test, y_test, 'Random Oversampling')
+elif sampler_choice == 3:
+    over = imb.over_sampling.RandomOverSampler(sampling_strategy=0.10)  # NOT UP-TO-DATE
+    under = imb.under_sampling.RandomUnderSampler(sampling_strategy=0.04166666666)  # NOT UP-TO-DATE
+    steps = [('o', over), ('u', under)]
+    pipeline = imb.pipeline.Pipeline(steps)
+    X, y = pipeline.fit_resample(X, y)
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    d2g.xg_boost(x_train, y_train, x_test, y_test, 'Random Over+Undersampling')
 
-print(score)
-print(conf_matrix)
