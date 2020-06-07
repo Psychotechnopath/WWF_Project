6e75dbb9bc69c6f988619c22217d4098a2224779
Tim J <timjongenelen@hotmail.com>
2020-06-07 17:46:48 +0200

    Laatste versie van random sampling v3

--- RAND_SAMP.py
+++ RAND_SAMP.py
@@ -11,30 +11,28 @@ with open(path, "rb") as f:
     y = pickle.load(f)
 X = df.copy(deep=True)
 y = y['future_deforestation']
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=47, stratify=y)
 
 
 sampler_choice = 2  # 1 = Under, 2 = Over, 3 = Both
-#TODO: Run again, modify so that you do not resample entire data but only training data
+sampling_strat = 1
 if sampler_choice == 1:
-    under = imb.under_sampling.RandomUnderSampler(sampling_strategy=0.04166666666)  # 4% minority after resampling
+    under = imb.under_sampling.RandomUnderSampler(sampling_strategy=sampling_strat)  # 4% minority after resampling
     steps = [('u', under)]
     pipeline = imb.pipeline.Pipeline(steps)
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=47, stratify=y)
-    X, y = pipeline.fit_resample(X, y)
+    x_train, y_train = pipeline.fit_resample(x_train, y_train)
     d2g.xg_boost(x_train, y_train, x_test, y_test, 'Random Undersampling')
 elif sampler_choice == 2:
-    over = imb.over_sampling.RandomOverSampler(sampling_strategy=0.04166666666)  # 4% minority after resampling
+    over = imb.over_sampling.RandomOverSampler(sampling_strategy=sampling_strat)  # 4% minority after resampling
     steps = [('o', over)]
     pipeline = imb.pipeline.Pipeline(steps)
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=47, stratify=y)
-    X, y = pipeline.fit_resample(X, y)
+    x_train, y_train = pipeline.fit_resample(x_train, y_train)
     d2g.xg_boost(x_train, y_train, x_test, y_test, 'Random Oversampling')
 elif sampler_choice == 3:
     over = imb.over_sampling.RandomOverSampler(sampling_strategy=0.10)  # NOT UP-TO-DATE
-    under = imb.under_sampling.RandomUnderSampler(sampling_strategy=0.04166666666)  # NOT UP-TO-DATE
+    under = imb.under_sampling.RandomUnderSampler(sampling_strategy=sampling_strat)  # NOT UP-TO-DATE
     steps = [('o', over), ('u', under)]
     pipeline = imb.pipeline.Pipeline(steps)
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=47, stratify=y)
-    X, y = pipeline.fit_resample(X, y)
+    x_train, y_train = pipeline.fit_resample(x_train, y_train)
     d2g.xg_boost(x_train, y_train, x_test, y_test, 'Random Over+Undersampling')
 
